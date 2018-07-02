e88e84d491c07cb3c72002a86040ba5a04fce90b
Dmitriy <textoviy@yandex.ru>
2018-07-02 22:58:02 +0700

    history.php is_auth fix

--- history.php
+++ history.php
@@ -2,6 +2,27 @@
 require_once 'templates/functions.php';
 require_once  'templates/lots_list.php';
 
+session_start();
+
+if (isset($_SESSION['user'])) {
+    $is_auth = true;
+    $user_name = $_SESSION['user']['name'];
+    $user_avatar = $_SESSION['user']['avatar'] ? 'img/uploads/users/' . $_SESSION['user']['avatar']: 'img/user.jpg';
+} else {
+    http_response_code(403);
+
+    $page_content = renderTemplate('templates/add_403.php', []);
+
+    $layout_content = renderTemplate('templates/layout.php', [
+        'main_title' => $page_title,
+        'categories' => $categories,
+        'content' => $page_content
+    ]);
+
+    print($layout_content);
+    exit(0);
+}
+
 $categories = [
     "Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"
 ];
